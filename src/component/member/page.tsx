7796d495174a1789616ac894b0f33535f09f6b99
bingual <bingual76@gmail.com>
2023-12-20 15:44:46 +0900

    다크모드, UI 추가

--- src/component/member/page.tsx
+++ src/component/member/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { signOut, useSession } from 'next-auth/react';
-import { Button } from 'flowbite-react';
+import Link from 'next/link';
 
 export default function MemberComponent() {
     const { data: session, status } = useSession();
@@ -22,18 +22,92 @@ export default function MemberComponent() {
                                     [일반회원] 입니다.
                                 </span>
                             </div>
-                            <Button
-                                onClick={async () => {
-                                    await signOut({
-                                        callbackUrl: '/',
-                                        redirect: true,
-                                    });
-                                }}
-                                className={'w-full'}
-                                color={'dark'}
-                            >
-                                로그아웃
-                            </Button>
+                            <ul>
+                                <li>
+                                    <span>쿠폰</span>
+                                    <p>
+                                        <Link href={'#'}>0</Link>
+                                    </p>
+                                </li>
+                                <li>
+                                    <span>적립금</span>
+                                    <p>
+                                        <Link href={'#'}>0원</Link>
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={'sec_02'}>
+                            <div className={'title'}>
+                                <h3>
+                                    {'주문처리 현황 '}
+                                    <span>(최근 3개월 기준)</span>
+                                </h3>
+                                <Link className={'btnNormal'} href={'#'}>
+                                    취소교환반품 내역
+                                </Link>
+                            </div>
+                            <ul>
+                                <li>
+                                    <p>입금전</p>
+                                    <Link href={'#'}>0</Link>
+                                </li>
+                                <li>
+                                    <p>배송준비중</p>
+                                    <Link href={'#'}>0</Link>
+                                </li>
+                                <li>
+                                    <p>배송중</p>
+                                    <Link href={'#'}>0</Link>
+                                </li>
+                                <li>
+                                    <p>배송완료</p>
+                                    <Link href={'#'}>0</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={'sec_03'}>
+                            <ul>
+                                <li>
+                                    <Link href={'#'}>주문내역 조회</Link>
+                                </li>
+                                <li>
+                                    <Link href={'#'}>적립금 내역</Link>
+                                </li>
+                                <li>
+                                    <Link href={'#'}>쿠폰 내역</Link>
+                                </li>
+                                <li>
+                                    <Link href={'#'}>쿠폰</Link>
+                                </li>
+                                <li>
+                                    <Link href={'#'}>최근 본 상품</Link>
+                                </li>
+                                <li>
+                                    <Link href={'#'}>나의 위시리스트</Link>
+                                </li>
+                                <li>
+                                    <Link href={'#'}>나의 게시글</Link>
+                                </li>
+                                <li>
+                                    <Link href={'#'}>회원 정보 수정</Link>
+                                </li>
+                                <li>
+                                    <Link href={'#'}>배송지 관리</Link>
+                                </li>
+                                <li>
+                                    <button
+                                        onClick={async () => {
+                                            await signOut({
+                                                callbackUrl: '/',
+                                                redirect: true,
+                                            });
+                                        }}
+                                    >
+                                        로그 아웃
+                                    </button>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
