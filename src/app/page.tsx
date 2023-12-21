c19edd5329d310ff20ef85a452e30cdba44962df
bingual <bingual76@gmail.com>
2023-12-22 01:08:58 +0900

    MainBanner, HotItem css and tag add #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use server';
-import HomeMainBanner from '@/component/home/mainBanner';
 import HomeHotItem from '@/component/home/hotItem';
+import { Carousel } from 'flowbite-react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function Home() {
     return (
@@ -8,7 +10,33 @@ export default async function Home() {
             <div className={'main-wr'}>
                 {/* TODO swiper 혹은 다른 슬라이드 라이브러리 사용해서 나머지 작업해야됨. 다크모드까지 하면 힘드니 그건 이후에 생각하기 */}
                 {/* FIXME 외부 이미지 링크 걸려있는거 나중에 전부 크롤링해서 이미지 로컬 저장후 DB에 링크 삽입 후 Image 태그로 최적화해서 불러오기*/}
-                <HomeMainBanner />
+                <div className="h-[480px] max-sm:h-[362px]">
+                    <Carousel className={'bg-black rounded-none'} pauseOnHover>
+                        <Image
+                            src={'/home/mainBanner1.jpg'}
+                            width={1920}
+                            height={1920}
+                            alt="..."
+                        />
+                        <Image
+                            src={'/home/mainBanner2.jpg'}
+                            width={1920}
+                            height={1920}
+                            alt="..."
+                        />
+                        <Image
+                            src={'/home/mainBanner3.jpg'}
+                            width={1920}
+                            height={1920}
+                            alt="..."
+                        />
+                    </Carousel>
+                    <div className="more">
+                        <Link href={'/recommend'}>
+                            더 알아보기 <i className="xi-angle-right"></i>
+                        </Link>
+                    </div>
+                </div>
                 <HomeHotItem />
             </div>
         </>
--- src/component/home/hotItem.tsx
+++ src/component/home/hotItem.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { useState } from 'react';
+import Image from 'next/image';
 
 export default function HomeHotItem() {
     const [hotItemCount, setHotItemCount] = useState([
@@ -19,81 +20,84 @@ export default function HomeHotItem() {
                                     <div className="thumbnail">
                                         <div className="prdImg">
                                             <Link href="#">
-                                                <picture>
-                                                    <img
-                                                        src="//kkst.kr/web/product/big/202308/a6ad7ca8b3f71c18889e096c05d8f3ea.jpg"
-                                                        id=""
-                                                        alt="스쿱넥 롱슬리브 티셔츠 [블랙]"
-                                                    />
-                                                </picture>
+                                                <Image
+                                                    src={'/home/hotItem1.jpg'}
+                                                    width={1920}
+                                                    height={1920}
+                                                    alt={
+                                                        '디태처블 울 블레이저 [블랙]'
+                                                    }
+                                                />
                                                 <div className="over-bg">
-                                                    <picture>
-                                                        <img
-                                                            src="//kkst.kr/web/product/medium/202308/faa33f7b68cebc75c543eaefde12efbb.jpg"
-                                                            id="//kkst.kr/web/product/medium/202308/faa33f7b68cebc75c543eaefde12efbb.jpg"
-                                                            alt="스쿱넥 롱슬리브 티셔츠 [블랙]"
-                                                            loading="lazy"
-                                                        />
-                                                    </picture>
+                                                    <Image
+                                                        src={
+                                                            '/home/hotItem1.jpg'
+                                                        }
+                                                        width={1920}
+                                                        height={1920}
+                                                        alt={
+                                                            '디태처블 울 블레이저 [블랙]'
+                                                        }
+                                                    />
                                                 </div>
                                             </Link>
                                         </div>
                                     </div>
                                     <div className="description">
                                         <div className="flex-wr">
-                                            <Link href="#">메르고</Link>
+                                            <Link href="#">인사일런스</Link>
                                             <div className="name">
                                                 <Link href="#">
                                                     <span>
-                                                        스쿱넥 롱슬리브 티셔츠
+                                                        디태처블 울 블레이저
                                                         [블랙]
                                                         <br />
                                                     </span>
                                                 </Link>
                                             </div>
                                             <div className="price-wr">
-                                                <p className="rate">19%</p>
+                                                <p className="rate">55%</p>
                                                 <p className="price">
                                                     <span className="front unit">
                                                         ₩
                                                     </span>
-                                                    17,000
+                                                    89,000
                                                 </p>
                                                 <p className="origin_price">
                                                     <span className="front unit">
                                                         ₩
                                                     </span>
-                                                    21,000
+                                                    199,000
                                                 </p>
                                             </div>
                                             <div className="count">
                                                 <div className="like_wrap">
                                                     <div className="likeButton likePrd likePrd_353">
                                                         <button type="button">
-                                                            <picture>
-                                                                <img
-                                                                    src="/web/upload/icon_202311301421409400.png"
-                                                                    className="likePrdIcon"
-                                                                    alt="좋아요 등록 전"
-                                                                />
-                                                            </picture>
+                                                            {/*<picture>*/}
+                                                            {/*    <img*/}
+                                                            {/*        src="/web/upload/icon_202311301421409400.png"*/}
+                                                            {/*        className="likePrdIcon"*/}
+                                                            {/*        alt="좋아요 등록 전"*/}
+                                                            {/*    />*/}
+                                                            {/*</picture>*/}
                                                             <span>
                                                                 <span className="likePrdCount likePrdCount_353">
-                                                                    270
+                                                                    154
                                                                 </span>
                                                             </span>
                                                         </button>
                                                     </div>
                                                 </div>
                                                 <div className="review_wrap">
-                                                    <a href="/product/스쿱넥-롱슬리브-티셔츠-블랙/353/category/1/display/3/#prdReview">
+                                                    <Link href="#">
                                                         <span className="alpha_module_count_container">
                                                             <i className="xi-message-o"></i>
                                                             <span className="alpha_module_count">
-                                                                247
+                                                                6
                                                             </span>
                                                         </span>
-                                                    </a>
+                                                    </Link>
                                                 </div>
                                             </div>
                                         </div>
