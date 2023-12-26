a31090983da9c705819dd62bcf04883cb31a664e
bingual <bingual76@gmail.com>
2023-12-27 02:47:27 +0900

    Python Automation Tool #2

--- crawling.py
+++ crawling.py
@@ -5,24 +5,21 @@ import urllib.request
 import time
 import os
 
-# TODO: 현재 필요한 부분만 부분 구현. 나중에 DB에 데이터 넣을 떄 퍼펫티어 사용할지 셀레니움 사용할지 선택해야됨.
+
+"""
+*주의* 
+Swiper Autoplay Option 적용된거는 슬라이드가 동적이기에 배열로 미리 정의 해서 반자동으로 짠 다음 가져오든지 해야함.
+
+수정 해야될 목록
+home page - mainBanner, recommend
+recommend page
+"""
 
 
-def brand_img(provider):
-    if "brandThum" not in provider and "prodThum" not in provider:
-        return
-
+def img_crawling(main_pv, sub_pv, url, css_selector):
     driver = webdriver.Chrome()
-    driver.get("https://kkst.kr/brand/")
+    driver.get(url)
     time.sleep(3)
-
-    css_selector = (
-        "div.brand-title > a > div.left-area > div > img"
-        if "brandThum" in provider
-        else "div.thumbnail > a > img"
-        if "prodThum" in provider
-        else None
-    )
 
     images = driver.find_elements(By.CSS_SELECTOR, css_selector)
     img_urls = []
@@ -31,13 +28,7 @@ def brand_img(provider):
         url = image.get_attribute("src")
         img_urls.append(url)
 
-    img_folder = (
-        "./public/brand/brandThum"
-        if "brandThum" in provider
-        else "./public/brand/prodThum"
-        if "prodThum" in provider
-        else None
-    )
+    img_folder = f"public/{main_pv}/{sub_pv}"
 
     if not os.path.isdir(img_folder):
         os.makedirs(img_folder)
@@ -48,40 +39,39 @@ def brand_img(provider):
     driver.quit()
 
 
-def home_img(provider):
-    if ["hotItem"] not in provider:
-        return
-
-    driver = webdriver.Chrome()
-    driver.get("https://kkst.kr/")
-    time.sleep(3)
-
-    anchor_box_id = [353, 352, 821, 823, 603, 822, 830, 824, 833, 232, 628, 580]
-    img_urls = []
-    for box_id in anchor_box_id:
-        css_selector = f"#anchorBoxId_{box_id} > div.thumbnail > div.prdImg > a > img"
-        images = driver.find_elements(By.CSS_SELECTOR, css_selector)
-
-        for image in images:
-            url = image.get_attribute("src")
-            img_urls.append(url)
-
-    img_folder = "public/home/hotItem"
-
-    if not os.path.isdir(img_folder):
-        os.makedirs(img_folder)
-
-    for idx, link in enumerate(img_urls):
-        urllib.request.urlretrieve(link, f"{img_folder}/item_{idx+1}.jpg")
-
-    driver.quit()
-
-
-provider_dict = {
-    "brand": {"brandThum": "brandThum", "prodThum": "prodThum"},
+mainPv = {"home": "home", "recommend": "recommend", "brand": "brand"}
+subPv = {
     "home": {
+        "mainBanner": "mainBanner",
+        "recommend": "recommend",
         "hotItem": "hotItem",
     },
+    "brand": {"brandThum": "brandThum"},
 }
-# brand_img(provider_dict.get("brand").get("prodThum"))
-# home_img(provider_dict.get("home").get("hotItem"))
+urls = {
+    "home": "https://kkst.kr/",
+    "recommend": "https://kkst.kr/recommend2/",
+    "brand": "https://kkst.kr/brand/",
+}
+cssSelectors = {
+    "home": {
+        "mainBanner": "div.main-wr div.main-slide div.swiper-slide a > img",
+        "recommend": "div.main-wr div.recommend2-wr div.item div.ec-base-product ul.prdList > li.xans-record- > div.thumbnail > div.prdImg > a > img",
+        "hotItem": "div.main-wr div.main-product-02 div.ec-base-product ul.prdList > li.xans-record- > div.thumbnail > div.prdImg > a > img",
+    },
+    "brand": {
+        "brandThum": "div.brand-wr div.brand-list-area div.brand-item div.brand-thum > img"
+    },
+}
+
+arg1 = mainPv["home"]
+arg2 = subPv[arg1]["hotItem"]
+arg3 = urls[arg1]
+arg4 = cssSelectors[arg1][arg2]
+
+img_crawling(
+    arg1,
+    arg2,
+    arg3,
+    arg4,
+)
