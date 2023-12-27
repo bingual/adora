40db6edee15c24e3f2f658fd59bf2d028be4b002
bingual <bingual76@gmail.com>
2023-12-27 18:42:08 +0900

    Python Automation Tool #3

--- crawling.py
+++ crawling.py
@@ -1,18 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import urllib.request
-import time
 import os
+import time
+import urllib.request
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 """
 *주의* 
-Swiper Autoplay Option 적용된거는 슬라이드가 동적이기에 배열로 미리 정의 해서 반자동으로 짠 다음 가져오든지 해야함.
+Swiper Autoplay Option 적용된거는 동적이고 중복 때문에 요소를 가져오기가 힘들어서 XPath로 되어 있기 때문에 변동사항에 따라 수정해줘야함.
+다만 프로젝트 특성상 일회성으로만 사용할거고 시시 때때로 변하기도하니깐 데이터 다 집어넣고나면 쓸일 없을거 같음. 
+어차피 나만 쓰는 개인용 자동화 툴 이니깐 고도화는 대충진행.
 
-수정 해야될 목록
-home page - mainBanner, recommend
-recommend page
+*차후 진행*
+상품 평균 20개 * 브랜드 30개로 가정할 시 데이터만 600개 가량 넣어야 하고 좋아요 댓글은 크롤링 할 필요 없음.
+퍼블리싱 작업 끝나면 DB 구성 한 다음 전부 다 크롤링해서 데이터 가져오고 인기 정렬은 전체 판매량 카운트 및 해당월의 상품 판매량 집계해서 처리 해야함.
 """
 
 
@@ -21,12 +22,32 @@ def img_crawling(main_pv, sub_pv, url, css_selector):
     driver.get(url)
     time.sleep(3)
 
-    images = driver.find_elements(By.CSS_SELECTOR, css_selector)
     img_urls = []
-
-    for image in images:
-        url = image.get_attribute("src")
-        img_urls.append(url)
+    if sub_pv in "mainBanner":
+        for i in range(7):
+            re_css_selector = css_selector.replace("div[2]", f"div[{2 + i}]")
+            images = driver.find_elements(
+                By.XPATH,
+                re_css_selector,
+            )
+            for image in images:
+                url = image.get_attribute("src")
+                img_urls.append(url)
+    elif sub_pv in "recommend":
+        for i in range(12):
+            re_css_selector = css_selector.replace("li[4]", f"li[{4 + i}]")
+            images = driver.find_elements(
+                By.XPATH,
+                re_css_selector,
+            )
+            for image in images:
+                url = image.get_attribute("src")
+                img_urls.append(url)
+    else:
+        images = driver.find_elements(By.CSS_SELECTOR, css_selector)
+        for image in images:
+            url = image.get_attribute("src")
+            img_urls.append(url)
 
     img_folder = f"public/{main_pv}/{sub_pv}"
 
@@ -39,33 +60,50 @@ def img_crawling(main_pv, sub_pv, url, css_selector):
     driver.quit()
 
 
-mainPv = {"home": "home", "recommend": "recommend", "brand": "brand"}
+mainPv = {
+    0: "home",
+    1: "discount",
+    2: "recommend",
+    3: "brand",
+}
 subPv = {
     "home": {
-        "mainBanner": "mainBanner",
-        "recommend": "recommend",
-        "hotItem": "hotItem",
+        0: "mainBanner",
+        1: "recommend",
+        2: "hotItem",
     },
-    "brand": {"brandThum": "brandThum"},
+    "discount": {0: "mainThum", 1: "subThum"},
+    "brand": {0: "brandThum", 1: "prodThum"},
 }
 urls = {
     "home": "https://kkst.kr/",
+    "discount": "https://kkst.kr/recommend/",
     "recommend": "https://kkst.kr/recommend2/",
     "brand": "https://kkst.kr/brand/",
 }
 cssSelectors = {
     "home": {
-        "mainBanner": "div.main-wr div.main-slide div.swiper-slide a > img",
-        "recommend": "div.main-wr div.recommend2-wr div.item div.ec-base-product ul.prdList > li.xans-record- > div.thumbnail > div.prdImg > a > img",
+        "mainBanner": "/html/body/div[1]/div[1]/div[4]/div/div/div[1]/div[1]/div[2]/a/img",
+        "recommend": "/html/body/div[1]/div[1]/div[4]/div/div/div[4]/div[2]/div[1]/ul/li[4]/div[1]/div[1]/a/img",
         "hotItem": "div.main-wr div.main-product-02 div.ec-base-product ul.prdList > li.xans-record- > div.thumbnail > div.prdImg > a > img",
     },
+    "discount": {
+        "mainThum": "div.recommend-wr div.main-product-03 div.ec-base-product ul.prdList > li.xans-record- > div.sale-img > a > img",
+        "subThum": "div.recommend-wr div.main-product-03 div.ec-base-product ul.prdList > li.xans-record- > div.sale-grid > div.thumbnail > div.prdImg > a > img",
+    },
     "brand": {
-        "brandThum": "div.brand-wr div.brand-list-area div.brand-item div.brand-thum > img"
+        "brandThum": "div.brand-wr div.brand-list-area div.brand-item div.brand-thum > img",
+        "prodThum": "div.brand-wr div.brand-list-area div.brand-item.item01 ul.brand-product-wr > li > div.thumbnail > a > img",
     },
 }
 
-arg1 = mainPv["home"]
-arg2 = subPv[arg1]["hotItem"]
+print(list(mainPv.items()))
+inp1 = int(input("Select main go on: "))
+print(list(subPv.items()))
+inp2 = int(input("Select sub go on: "))
+
+arg1 = mainPv[inp1]
+arg2 = subPv[arg1][inp2]
 arg3 = urls[arg1]
 arg4 = cssSelectors[arg1][arg2]
 
